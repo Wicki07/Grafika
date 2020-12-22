678e97e03b8ddd340c3073c1a7d5201a64f35d28
Mateusz Wicki <71140843+Wicki07@users.noreply.github.com>
2020-12-22 23:18:15 +0100

    zrobienie canvas

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb2_c = new System.Windows.Forms.TextBox();
+            this.tb2_b = new System.Windows.Forms.TextBox();
+            this.tb2_a = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.canvas.Location = new System.Drawing.Point(1, 79);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(800, 400);
+            this.canvas.TabIndex = 0;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.x_Change);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "RysujFunkcje";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb2_c
+            // 
+            this.tb2_c.Location = new System.Drawing.Point(329, 34);
+            this.tb2_c.Margin = new System.Windows.Forms.Padding(2);
+            this.tb2_c.Name = "tb2_c";
+            this.tb2_c.Size = new System.Drawing.Size(25, 20);
+            this.tb2_c.TabIndex = 6;
+            this.tb2_c.Text = "1";
+            this.tb2_c.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tb2_b
+            // 
+            this.tb2_b.Location = new System.Drawing.Point(282, 34);
+            this.tb2_b.Margin = new System.Windows.Forms.Padding(2);
+            this.tb2_b.Name = "tb2_b";
+            this.tb2_b.Size = new System.Drawing.Size(25, 20);
+            this.tb2_b.TabIndex = 7;
+            this.tb2_b.Text = "1";
+            this.tb2_b.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tb2_a
+            // 
+            this.tb2_a.Location = new System.Drawing.Point(224, 34);
+            this.tb2_a.Margin = new System.Windows.Forms.Padding(2);
+            this.tb2_a.Name = "tb2_a";
+            this.tb2_a.Size = new System.Drawing.Size(25, 20);
+            this.tb2_a.TabIndex = 8;
+            this.tb2_a.Text = "1";
+            this.tb2_a.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(308, 36);
+            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(21, 13);
+            this.label13.TabIndex = 3;
+            this.label13.Text = "x +";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 36);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "x^2 +";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 36);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "y =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 481);
+            this.Controls.Add(this.tb2_c);
+            this.Controls.Add(this.tb2_b);
+            this.Controls.Add(this.tb2_a);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb2_c;
+        private System.Windows.Forms.TextBox tb2_b;
+        private System.Windows.Forms.TextBox tb2_a;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        int xMax = 10;
+        int yMax = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            //draw x,y Axis
+            var x1 = canvas.Width / 2;
+            var y1 = canvas.Height / 2;
+
+
+            var g = canvas.CreateGraphics();
+            var blackPen = new Pen(Color.Black, 1f);
+            g.DrawLine(blackPen, 0, y1, canvas.Width, y1);
+            g.DrawLine(blackPen, x1, 0, x1, canvas.Height);
+
+            //define and draw unitX, unitmark
+
+            int unitDividend = 10;
+            int count = 0;
+            int heightOfMark = 10;
+ 
+            float addX = ((float)x1 / (float)xMax) * 0.9f;
+            float addY = ((float)y1 / (float)yMax) * 0.9f;
+
+            // marks on x
+            for (float x = x1; x <= canvas.Width; x += addX) //x: relative value without U
+            {
+                if (x != x1)
+                {
+                    if (count % 10 == 0)
+                    {
+                        g.DrawLine(blackPen, x, y1, x, y1 - heightOfMark); // 1u, 2u, 3u  
+
+                        //label
+                        string text = (count).ToString();
+                        if (text != "0")
+                        {
+                            Label l = new Label();
+                            l.Text = text;
+                            l.AutoSize = true;
+                            l.Location = new Point((int)(x - l.Width / 11), (int)(y1 + l.Height / 5));
+                            l.Parent = canvas;
+                        }
+                    }
+                    else if (count % 5 == 0) g.DrawLine(blackPen, x, y1, x, y1 - heightOfMark * 2 / 3);
+                    else if (xMax < 60) g.DrawLine(blackPen, x, y1, x, y1 - heightOfMark / 3);
+                }
+                count++;
+            }
+            count = 0;
+            for (float x = x1; x >= 0; x -= addX)
+            {
+                if (x != 0)
+                {
+                    if (count % 10 == 0)
+                    {
+                        g.DrawLine(blackPen, x, y1, x, y1 - heightOfMark); // 1u, 2u, 3u  
+
+                        //label
+                        string text = (count).ToString();
+                        if (text != "0")
+                        {
+                            Label l = new Label();
+                            l.Text = text;
+                            l.AutoSize = true;
+                            l.Location = new Point((int)(x - l.Width / 11), (int)(y1 + l.Height / 5));
+                            l.Parent = canvas;
+                        }
+                    }
+                    else if (count % 5 == 0) g.DrawLine(blackPen, x, y1, x, y1 - heightOfMark * 2 / 3);
+                    else if (xMax < 60) g.DrawLine(blackPen, x, y1, x, y1 - heightOfMark / 3);
+                }
+                count++;
+            }
+
+            //define and draw unitY, unitmarks
+            count = 0;
+            for (float y = y1; y <= canvas.Height; y += addY)
+            {
+                if (y != y1)
+                {
+                    if (count % 10 == 0)
+                    {
+                        g.DrawLine(blackPen, x1, y, x1 + heightOfMark, y); // 1u, 2u, 3u  
+
+                        //label
+                        string text = (count).ToString();
+                        if (text != "0")
+                        {
+                            Label l = new Label();
+                            l.Text = text;
+                            l.AutoSize = true;
+                            l.Location = new Point((int)(x1 - l.Width / 3), (int)(y - l.Height / 5));
+                            l.Parent = canvas;
+                        }
+                    }
+                    else if (count % 5 == 0) g.DrawLine(blackPen, x1, y, x1 + heightOfMark * 2 / 3, y);
+                    else if (xMax < 60) g.DrawLine(blackPen, x1, y, x1 + heightOfMark / 3, y);
+                }
+                count++;
+            }
+            count = 0;
+            for (float y = y1; y >= 0; y -= addY)
+            {
+                if (y != y1)
+                {
+                    if (count % 10 == 0)
+                    {
+                        g.DrawLine(blackPen, x1, y, x1 + heightOfMark, y); // 1u, 2u, 3u  
+
+                        //label
+                        string text = (count).ToString();
+                        if (text != "0")
+                        {
+                            Label l = new Label();
+                            l.Text = text;
+                            l.AutoSize = true;
+                            l.Location = new Point((int)(x1 - l.Width / 3), (int)(y - l.Height / 5));
+                            l.Parent = canvas;
+                        }
+                    }
+                    else if (count % 5 == 0) g.DrawLine(blackPen, x1, y, x1 + heightOfMark * 2 / 3, y);
+                    else if (xMax < 60) g.DrawLine(blackPen, x1, y, x1 + heightOfMark / 3, y);
+                }
+                count++;
+            }
+        }
+        
+        private void x_Change(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if(e.Delta > 0)
+            {
+                xMax++;
+                yMax++;
+            }
+            else
+            {
+                yMax--;
+                xMax--;
+            }
+            var label = this.canvas.Controls.OfType<Label>();
+            foreach(Label l in label)
+            {
+                this.canvas.Controls.Remove(l);
+                //l.Dispose();
+            }
+            canvas.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(tb2_a.Text);
+            int b = int.Parse(tb2_b.Text);
+            int c = int.Parse(tb2_c.Text);
+            float delta = b * b - 4 * a * c;
+            if(delta < 0)
+            {
+                xMax = 20;
+            }
+            else if(delta == 0)
+            {
+                xMax = (-(b)) / (2 * a);
+            }
+            else
+            {
+                var x1 = ((-(b)) - Math.Sqrt(delta)) / (2 * a);
+                var x2 = ((-(b)) + Math.Sqrt(delta)) / (2 * a);
+                if (x1 > x2)
+                {
+                    xMax = (int)x1;
+                }
+                else xMax = (int)x2;
+            }
+            var label = this.canvas.Controls.OfType<Label>();
+            foreach (Label l in label)
+            {
+                this.canvas.Controls.Remove(l);
+                //l.Dispose();
+            }
+            canvas.Refresh();
+        }
+    }
+}
